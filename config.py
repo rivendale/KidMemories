5c9ebb83ac052151abb646008babbbc00c5375a8
Nick <nick.ryg@gmail.com>
2021-02-15 17:35:03 -0500

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'kgdjfkdfj3sdf4905uikgfdjgFGFDKLJ'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'AddSecretKeyHere#$%^&'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'LLTRK.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
@@ -17,5 +17,5 @@ class Config(object):
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['nickryg@gmail.com']
+    ADMINS = ['admin@admin.com']
     POSTS_PER_PAGE = 25
